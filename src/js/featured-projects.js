e20e619a2272635ea8aa9e657f38ed5ed9b38611
McKenna Schaller <mkschaller4h@gmail.com>
2019-02-09 10:07:54 -0600

    site progress

--- src/js/featured-projects.js
+++ src/js/featured-projects.js
@@ -1,35 +1,35 @@
-// get array of .project items
-var project = document.querySelectorAll('.project div');
 
-if (project != null) {
-    project.forEach(function(project) {
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-        // select elements
-        var image = project.querySelector('img');
-        
-        // only make background changes in divs that contain an image
-        if (image != null) {
-            var src = image.getAttribute('src');
-    
-            // take image src and set as background with positioning according to the image
-            if (src.includes('bauhaus')) {
-                project.style.background = 'url("' + src + '") no-repeat top center';
-            } else {
-                project.style.background = 'url("' + src + '") no-repeat center center';
-            }
+    // get array of .project items
+    var project = document.querySelectorAll('.project div');
+
+    if (project != null) {
+
+        project.forEach(function(project) {
+
+            project.style.opacity = '1';
+
+            // select elements
+            var image = project.querySelector('img');
             
-            project.style.backgroundSize = 'cover';
+            // only make background changes in divs that contain an image
+            if (image != null) {
+                var src = image.getAttribute('src');
         
-            // // hover state
-            // project.addEventListener("mouseover", function(event) {
-            //     project.style.scale = 'transform: scale(1.2)';
-            //     project.style.transition = 'all .5s';
+                // take image src and set as background with positioning according to the image
+                if (src.includes('bauhaus') || src.includes('garamont')) {
+                    project.style.background = 'url("' + src + '") no-repeat top center';
+                } else {
+                    project.style.background = 'url("' + src + '") no-repeat center center';
+                }
                 
-            // });
+                project.style.backgroundSize = 'cover';
             
-            // hide image
-            image.style.display = 'none';
-        }
-    });
-}
+                // hide image
+                image.style.display = 'none';
+            }
+        });
+    }
 
+});
